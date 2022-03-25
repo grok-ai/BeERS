9854533e779252bee2b5517f48be57df3afcd8eb
Valentino Maiorca <valentino@maiorca.xyz>
2022-03-25 21:55:33 +0100

    [Worker] Fix GPU detection by removing ID truncation
    Signed-off-by: Valentino Maiorca <valentino@maiorca.xyz>

--- worker_setup.sh
+++ worker_setup.sh
@@ -8,22 +8,14 @@ sudo mkdir -p /etc/systemd/system/docker.service.d
 # https://stackoverflow.com/a/17841619
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-GPU_IDS=$(nvidia-smi -a | grep UUID | awk '{print substr($4,0,12)}')
-#GPU_IDS=$(awk '{print substr($4,0,12)}' multi_gpu)
+GPU_IDS=$(nvidia-smi -a | grep UUID | awk '{print $4}')
 
 gpu_resources=()
 for gpu_id in $GPU_IDS; do
-#    gpu_resources_string="$gpu_resources_string--node-generic-resource gpu=${gpu_id} "
-    gpu_resources+=("\"NVIDIA-GPU=$gpu_id\"")
+    gpu_resources+=("\"GPU=$gpu_id\"")
 done
 
 gpus_list=$(join_by , "${gpu_resources[@]}")
-# TODO: --append?
-#cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
-#[Service]
-#ExecStart=
-#ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-container-toolkit
-#EOF
 
 # This solution needs nvidia-container-runtime to be available!
 cat <<EOF | sudo tee /etc/docker/daemon.json
